80be227be25437a2a618ea3cff29856322b8b075
Lucas Miguel <lucasmiguelseven@gmail.com>
2026-05-28 21:49:55 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,17 @@
+// =========================
+// LOGIN
+// =========================
 
+// Pega o formulário do HTML
+const formulario = document.querySelector("form");
+
+// Escuta quando o formulário for enviado
+formulario.addEventListener("submit", function(event) {
+
+  // Impede a página de recarregar
+  event.preventDefault();
+
+  // Mensagem de sucesso
+  alert("Login realizado com sucesso!");
+
+});
