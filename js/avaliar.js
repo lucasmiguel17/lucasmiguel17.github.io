c62b78bbf16d87f3758c03a79e79c0c04d664c4c
Lucas Miguel <lucasmiguelseven@gmail.com>
2026-06-03 23:58:55 -0300

    Update avaliar.js

--- js/avaliar.js
+++ js/avaliar.js
@@ -11,3 +11,35 @@ if (produto) {
 } else {
     titulo.textContent = "Nova avaliação";
 }
+
+// AVALIAÇÃO
+
+const estrelas = document.querySelectorAll(".estrela");
+const notaTexto = document.getElementById("nota-escolhida");
+const notaInput = document.getElementById("nota");
+
+let notaAtual = 0;
+
+estrelas.forEach((estrela, indice) => {
+
+    estrela.addEventListener("click", () => {
+
+        notaAtual = indice + 1;
+
+        notaInput.value = notaAtual;
+
+        notaTexto.textContent = `Nota: ${notaAtual}/5`;
+
+        estrelas.forEach((e, i) => {
+
+            if (i < notaAtual) {
+                e.classList.add("ativa");
+            } else {
+                e.classList.remove("ativa");
+            }
+
+        });
+
+    });
+
+});
